5bce90fa8aba189e461965f5059709b230ba6013
Jinlei Li <jinleili0@outlook.com>
2023-05-01 13:11:06 +0800

    过滤第二章代码在浏览器端的控制台干扰消息

--- code/beginner/tutorial2-surface/src/main.rs
+++ code/beginner/tutorial2-surface/src/main.rs
@@ -132,53 +132,8 @@ impl State {
     }
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub async fn run() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Could't initialize logger");
-        } else {
-            env_logger::init();
-        }
-    }
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        use winit::dpi::PhysicalSize;
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                match doc.get_element_by_id("wasm-example") {
-                    Some(dst) => {
-                        window.set_inner_size(PhysicalSize::new(450, 400));
-                        let _ = dst.append_child(&web_sys::Element::from(window.canvas()));
-                    }
-                    None => {
-                        window.set_inner_size(PhysicalSize::new(800, 800));
-                        let canvas = window.canvas();
-                        canvas.style().set_css_text(
-                            "background-color: black; display: block; margin: 20px auto;",
-                        );
-                        doc.body().and_then(|body| {
-                            Some(body.append_child(&web_sys::Element::from(canvas)))
-                        });
-                    }
-                };
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-    }
-
-    // State::new uses async code, so we're going to wait for it to finish
-    let mut state = State::new(&window).await;
-
+fn start_event_loop(state: State, window: winit::window::Window, event_loop: EventLoop<()>) {
+    let mut state = state;
     event_loop.run(move |event, _, control_flow| {
         match event {
             Event::WindowEvent {
@@ -228,6 +183,82 @@ pub async fn run() {
             _ => {}
         }
     });
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub async fn run() {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Could't initialize logger");
+        } else {
+            env_logger::init();
+        }
+    }
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        wasm_bindgen_futures::spawn_local(async move {
+            // 在网页中，需要先添加 canvas 再初始化 State
+            use winit::dpi::PhysicalSize;
+            use winit::platform::web::WindowExtWebSys;
+            web_sys::window()
+                .and_then(|win| win.document())
+                .map(|doc| {
+                    match doc.get_element_by_id("wasm-example") {
+                        Some(dst) => {
+                            window.set_inner_size(PhysicalSize::new(450, 400));
+                            let _ = dst.append_child(&web_sys::Element::from(window.canvas()));
+                        }
+                        None => {
+                            window.set_inner_size(PhysicalSize::new(800, 800));
+                            let canvas = window.canvas();
+                            canvas.style().set_css_text(
+                                "background-color: black; display: block; margin: 20px auto;",
+                            );
+                            doc.body()
+                                .map(|body| body.append_child(&web_sys::Element::from(canvas)));
+                        }
+                    };
+                })
+                .expect("Couldn't append canvas to document body.");
+
+            // 然后在创建 State 实例
+            let state = State::new(&window).await;
+
+            let run_closure =
+                Closure::once_into_js(move || start_event_loop(state, window, event_loop));
+
+            // 处理运行过程中抛出的 JS 异常。
+            // 否则 wasm_bindgen_futures 队列将中断，且不再处理任何任务。
+            if let Err(error) = call_catch(&run_closure) {
+                let is_control_flow_exception =
+                    error.dyn_ref::<js_sys::Error>().map_or(false, |e| {
+                        e.message().includes("Using exceptions for control flow", 0)
+                    });
+
+                if !is_control_flow_exception {
+                    web_sys::console::error_1(&error);
+                }
+            }
+
+            #[wasm_bindgen]
+            extern "C" {
+                #[wasm_bindgen(catch, js_namespace = Function, js_name = "prototype.call.call")]
+                fn call_catch(this: &JsValue) -> Result<(), JsValue>;
+            }
+        });
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        // State::new uses async code, so we're going to wait for it to finish
+        let state = State::new(&window).await;
+        start_event_loop(state, window, event_loop);
+    }
 }
 
 fn main() {
