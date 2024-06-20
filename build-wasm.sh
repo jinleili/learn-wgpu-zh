7ffca067879dd593141979b1de50222f8c655927
Jinlei Li <jinleili0@outlook.com>
2024-06-20 16:31:21 +0800

    优化 wasm 包大小

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,6 +1,6 @@
 export RES_PATH=learn-wgpu-zh
-
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --no-default-features --release --target wasm32-unknown-unknown --features webgl \
+#
+cargo build --no-default-features --profile wasm-release --target wasm32-unknown-unknown --features webgl \
 --bin tutorial1-window \
 --bin tutorial2-surface \
 --bin tutorial3-pipeline \
@@ -15,14 +15,23 @@ RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --no-default-features --releas
 --bin tutorial12-camera
 
 # 只能使用 WebGPU 的示例程序
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --no-default-features --release --target wasm32-unknown-unknown \
+cargo build --no-default-features --profile wasm-release --target wasm32-unknown-unknown \
 --bin compute-pipeline \
 --bin vertex-animation \
 --bin hilbert-curve \
 --bin hdr 
 
 # Generate bindings
-for i in target/wasm32-unknown-unknown/release/*.wasm;
+for i in target/wasm32-unknown-unknown/wasm-release/*.wasm;
 do
-    wasm-bindgen --no-typescript --out-dir docs/public/wasm --web "$i";
+    wasm-bindgen --no-typescript --out-dir wasm --web "$i";
+    # 优化 wasm 包大小
+    filename=$(basename "$i");
+    # Remove the .wasm extension from filename
+    name_no_extension="${filename%.wasm}"
+    wasm-opt -Oz wasm/"$name_no_extension"_bg.wasm --output docs/public/wasm/"$name_no_extension"_bg.wasm;
+
+    cp wasm/"$name_no_extension".js docs/public/wasm/"$name_no_extension".js
 done
+
+
