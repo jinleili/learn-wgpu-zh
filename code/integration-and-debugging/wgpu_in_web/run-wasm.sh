e952bb32eacd06de573ce1d9b56b690640565749
jinleili <jinleili0@outlook.com>
2025-12-18 21:20:23 +0800

    Fix CI build failure

--- code/integration-and-debugging/wgpu_in_web/run-wasm.sh
+++ code/integration-and-debugging/wgpu_in_web/run-wasm.sh
@@ -1,9 +1,8 @@
 set -e 
-
-cargo build --no-default-features --features web_rwh --profile wasm-release --target wasm32-unknown-unknown 
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' cargo build --no-default-features --features web_rwh --profile wasm-release --target wasm32-unknown-unknown 
 
 # Generate bindings
-wasm-bindgen --no-typescript --out-dir wasm --web "../../../target/wasm32-unknown-unknown/debug/wgpu_in_web.wasm";
+wasm-bindgen --no-typescript --out-dir wasm --web "../../../target/wasm32-unknown-unknown/wasm-release/wgpu_in_web.wasm";
 
 cp wasm/wgpu_in_web.js public/wgpu_in_web.js
 cp wasm/wgpu_in_web_bg.wasm public/wgpu_in_web_bg.wasm
